2674f82369145573c10e4cbf1b68262b8fd45b8f
Monika <monika48912@gmail.com>
2018-01-31 11:13:28 +0530

    initial commit

---
+++ week1/problem1.py
@@ -0,0 +1,6 @@
+string=input("Enter string:")
+vowels=0
+for i in string:
+           if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u'):
+                vowels=vowels+1
+print('number of vowels is:',vowels)
