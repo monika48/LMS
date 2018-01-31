1441821b44a181f3f928155dafef315dcc05f04f
Monika <monika48912@gmail.com>
2018-01-31 11:21:38 +0530

    final commit

--- week1/problem1.py
+++ week1/problem1.py
@@ -1,3 +1,6 @@
+#Assume s is a string of lower case characters.
+
+#Write a program that counts up the number of vowels contained in the string s. Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl', your program should print:
 string=input("Enter string:")
 vowels=0
 for i in string:
--- week1/problem2.py
+++ week1/problem2.py
@@ -1,3 +1,6 @@
+#Assume s is a string of lower case characters.
+
+#Write a program that prints the number of times the string 'bob' occurs in s. For example, if s = 'azcbobobegghakl', then your program should print
 s=input("Enter string:")
 num= 0
 for i in range(1, len(s)-1):
